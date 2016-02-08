7d4b81bc010538e0399b25cba4c2d0bdfddf08a1
viciousviper <j_h@mail.org>
2016-02-08 23:44:30 +0100

    Initial commit

---
+++ CoverityTestbed/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Globalization;
+
+namespace CoverityTestbed
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Coverity!");
+            Console.WriteLine(string.Format(CultureInfo.CurrentCulture, "CLR version is {0}", Environment.Version));
+            Console.ReadLine();
+        }
+    }
+}
